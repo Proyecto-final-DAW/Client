252fb25f95bba9799a5ca7fa1ae9babd20ebe031
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2026-02-24 19:23:39 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { Landing } from './features/user/ui/components/landing/Landing';
 import { AuthProvider } from './context/AuthProvider';
 import { Login } from './features/user/ui/components/Login';
 import { ProtectedRoute } from './routes/ProtectedRoute';
 
 // Páginas temporales
-const Landing = () => <h1>Landing Page</h1>;
 const Dashboard = () => <h1>Dashboard</h1>;
 
 export const App = (): React.JSX.Element => {
