0dd56c0daa162836d4378c466f4c413d9b799277
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2026-03-30 14:27:23 +0200

    feature/dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { AuthProvider } from './context/AuthProvider';
+import { DashboardLayout } from './features/onboarding/ui/components/DashboardLayout';
 import OnboardingView from './features/onboarding/ui/OnboardingView';
 import { Landing } from './features/user/ui/components/landing/Landing';
 import { Login } from './features/user/ui/components/Login';
 import { ProtectedRoute } from './routes/ProtectedRoute';
-import { DashboardLayout } from './features/onboarding/ui/components/DashboardLayout';
 
 const Dashboard = () => <h1>Dashboard</h1>;
 
