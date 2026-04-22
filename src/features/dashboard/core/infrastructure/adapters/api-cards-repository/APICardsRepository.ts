ec6f62029c6bf1f5ade59d62a90fe4df6e2fa16b
Victorgonzalez-ces <victor.gonzalez@cesjuanpablosegundo.es>
2026-04-22 10:24:49 +0200

    fix: feature/cardsDashboard

--- src/features/dashboard/core/infrastructure/adapters/api-cards-repository/APICardsRepository.ts
+++ src/features/dashboard/core/infrastructure/adapters/api-cards-repository/APICardsRepository.ts
@@ -1,22 +1,23 @@
 import axios, { AxiosError } from 'axios';
 
+import { API_ENDPOINTS } from '../../../../../../config/api';
 import type { APIErrorResponse } from '../../../../../../shared/api/error-response/APIErrorResponse';
 import type { CardsRepository } from '../../../application/ports/CardsRepository';
 import type { Cards } from '../../../domain/models/Cards';
 import type { GetCardsDTO } from './dtos/GetCardsDTO';
 import { CardsFromDTO } from './mappers/CardsFromDTO';
 
-// need to change this once the api is available, now we use the mocked
-const CARDS_URL = `${import.meta.env.VITE_API_URL}/users/cards`;
-
 export class APICardsRepository implements CardsRepository {
   async getCards(token: string): Promise<Cards> {
     try {
-      const response = await axios.get<GetCardsDTO>(CARDS_URL, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.get<GetCardsDTO>(
+        API_ENDPOINTS.dashboardCards,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       return CardsFromDTO.fromDTO(response.data);
     } catch (error) {
